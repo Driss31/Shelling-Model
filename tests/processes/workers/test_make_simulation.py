a2f4ad2cf08edcd34c88d1a062f85e8036b07153
Driss Debbagh-Nour <dndriss@gmail.com>
2020-06-20 16:02:34 +0200

    Fix mypy issue

--- tests/processes/workers/test_make_simulation.py
+++ tests/processes/workers/test_make_simulation.py
@@ -1,2 +1,2 @@
 """Tests for worker make_simulation."""
-from ethnic_residential_dynamics.processes.workers.make_simulation import simulation
+# from ethnic_residential_dynamics.processes.workers.make_simulation import simulation
